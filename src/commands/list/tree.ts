aa83cc8a803d3bb5d8f3c7cc21d57d95808bda62
Elliott Marquez <5981958+e111077@users.noreply.github.com>
2024-11-20 15:48:07 -0800

    Enhance tree command to filter for commits of interest based on tags and branches

--- src/commands/list/tree.ts
+++ src/commands/list/tree.ts
@@ -18,12 +18,28 @@ function handler(): void {
     // Execute the tree command with color preservation
     const rawTree = execCommand(`
 MERGE_BASE=$(git merge-base --octopus $(git branch --format='%(refname)'))
+
 if git rev-parse $MERGE_BASE^ >/dev/null 2>&1; then
-    # Has parent - use ~.. to show from merge-base up
-    git -c color.ui=always log --simplify-by-decoration --decorate --oneline --graph --branches \${MERGE_BASE}~..
+    # Prints the git log, in the format of <hash> (decorations) and only keeps
+    # the commits that have tags, HEAD, a local branch, or a
+    # (origin/remote, local-name) decoration. Then cuts the decorations off.
+    COMMITS_OF_INTEREST=$(git log --simplify-by-decoration --decorate --oneline --branches \${MERGE_BASE}~..  --format="%h%d" | \
+        grep -E "^.+(tag)|(HEAD)|(\\([^/]+\\))|(\\([^/]+, ?origin/.+\\))|(\\(?origin/.+\\,[^/]+)).*$" | \
+        cut -d' ' -f1)
+else
+    # No parent (root commit) - use range to include merge-base. Find all commits that are only on remote branches
+    COMMITS_OF_INTEREST=$(git log --simplify-by-decoration --decorate --oneline --branches \${MERGE_BASE}^@  --format="%h%d" | \
+        grep -E "^.+(tag)|(HEAD)|(\\([^/]+\\))|(\\([^/]+, ?origin/.+\\))|(\\(?origin/.+\\,[^/]+)).*$" | \
+        cut -d' ' -f1)
+fi
+
+if git rev-parse $MERGE_BASE^ >/dev/null 2>&1; then
+    # Has parent - use ~.. to show from merge-base up. Then only keeps the
+    # commits of interest and the lines that don't have a commit (the branching)
+    git -c color.ui=always log --simplify-by-decoration --decorate --oneline --graph --branches \${MERGE_BASE}~.. | grep -E "$COMMITS_OF_INTEREST|(^[^*]*$)"
 else
     # No parent (root commit) - use range to include merge-base
-    git -c color.ui=always log --simplify-by-decoration --decorate --oneline --graph --branches \${MERGE_BASE}^@
+    git -c color.ui=always log --simplify-by-decoration --decorate --oneline --graph --branches \${MERGE_BASE}^@  | grep -E "$COMMITS_OF_INTEREST|(^[^*]*$)"
 fi`);
 
     const staleParentRegex = new RegExp(`tag:.+${makeStaleParentTag('(.+?)', '[0-9]+')}`, 'g');
